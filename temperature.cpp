6b20ffb614a297f02e5acd24764e69ca12ae59a7
sck22 <chaithanyakumar.ds@gmail.com>
2019-05-24 13:16:25 +0530

    learning

---
+++ temperature.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // converte temperature from fahrenheit to celsius.
+    double tempf = 69.0;
+    double tempc;
+
+    tempc = (tempf - 32)/1.8;
+    std::cout << "The temp is " << tempc << "degrees Celsius.\n";
+  
+  
+  
+}
---
+++ user_input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/*
+<< operator is used to set from
+>> operator is used to get from
+*/
+
+int main() {
+  
+  int tip = 0;
+  
+  std::cout << "Enter tip amount: ";
+  std::cin >> tip;
+  std::cout << "You paid " << tip << " dollars.\n";
+  
+  
+}
