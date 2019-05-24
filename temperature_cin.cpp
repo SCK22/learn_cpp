656f7d1c3e87b05d44dc820c1d5fc98ad76b2a84
sck22 <chaithanyakumar.ds@gmail.com>
2019-05-24 13:16:54 +0530

    learning

--- temperature_cin.cpp
+++ temperature_cin.cpp
@@ -8,7 +8,7 @@ int main() {
   // Ask the user
   
   std::cout << "Enter the temperature in Fahrenheit: ";
-  std::cin >> tempf;
+  std::cin >> tempf; // should add some checks in place , only float to be accepted.
   tempc = (tempf - 32) / 1.8;
   
   std::cout << "The temp is " << tempc << " degrees Celsius.\n";
